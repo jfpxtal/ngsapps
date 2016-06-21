7924a18c7b7414b2802baf8bdcfa3d55e0263a4f
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-06-21 10:34:55 +0200

    lagrange to py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,11 @@
 from libngsapps_utils import *
+
+from ngsolve import *
+
+def Lagrange(mesh, **args):
+    """
+    Create H1 finite element space with Lagrange basis.
+    documentation of arguments is available in FESpace.
+    """
+    fes = FESpace("lagrangefespace", mesh, **args)
+    return fes
