519c7bfff236187f9f7b0c25fcd548347ee1f4cf
jfpxtal <pietschm@wwu.de>
2017-03-03 09:54:13 +0100

    blub

--- nonlocal/nonlocaldiff_periodic.py
+++ nonlocal/nonlocaldiff_periodic.py
@@ -15,12 +15,12 @@ def sqr(x):
     return x*x
 
 # FEM parameters
-order = 2
-conv_order = 3
+order = 1
+conv_order = 2
 maxh = 0.17
 
 # time step and final time
-tau = 0.005
+tau = 0.00005
 tend = 25
 
 ngsglobals.msg_level = 1
@@ -28,7 +28,7 @@ ngsglobals.msg_level = 1
 vtkoutput = False
 
 geo = SplineGeometry()
-xmin, xmax, ymin, ymax = -3, 3, -3 3
+xmin, xmax, ymin, ymax = -3, 3, -3, 3
 dx = xmax - xmin
 dy = ymax - ymin
 pnts = [(xmin,ymin),(xmax,ymin),(xmax,ymax),(xmin,ymax)]
@@ -40,12 +40,12 @@ lright = geo.Append ( ["line", pnums[1], pnums[2]], bc="right")
 geo.Append ( ["line", pnums[0], pnums[3]], leftdomain=0, rightdomain=1, bc="left", copy=lright)
 geo.Append ( ["line", pnums[3], pnums[2]], leftdomain=0, rightdomain=1, bc="top", copy=lbot)
 
-mesh = Mesh(geo.GenerateMesh(maxh=maxh))
+mesh = Mesh(geo.GenerateMesh(maxh=0.1))
 
 # Convolution kernel
 thin = 10
-k0 = 1
-Kmax = 0.2
+k0 = 2
+Kmax = 0.5
 K = k0*exp(-thin*(sqr(x-xPar)+sqr(y-yPar)))
 K2 = k0*IfPos(1-(1/Kmax)*sqrt(sqr(x)+sqr(y)), 1-(1/Kmax)*sqrt(sqr(x)+sqr(y)), 0) # k0*exp(-thin*(sqr(x-xPar)+sqr(y-yPar)))
 Kint = Integrate(K2,mesh)
@@ -59,6 +59,10 @@ u, w = fes.TrialFunction(), fes.TestFunction()
 
 v = GridFunction (fes)
 g = GridFunction(fes)
+s = GridFunction(fes)
+q = s.vec.CreateVector()
+s1 = s.vec.CreateVector()
+rhs = s.vec.CreateVector()
 conv = ParameterLF(w*K, s, conv_order, repeat=1, patchSize=[dx, dy])
 
 # bilinear-forms
@@ -76,16 +80,18 @@ mmat = m.mat
 smat = mmat.CreateMatrix()
 
 # initial values
-s = GridFunction(fes)
 b0, b1 = 4, 7
 sig = 25
 #s.Set(b0*exp(-sig*(sqr(x-0.5)+sqr(y-0.5)))+b1*exp(-sig*(sqr(x+0.5)+sqr(y-0.5))))
 #s.Set(IfPos(RandomCF(0.0,1.0)+1,RandomCF(0.0,1.0)+1,0))
-s.Set(0.8 + 0.1*RandomCF(0.0,1.0))
+s.Set(0.8 + 0.0*RandomCF(0.0,1.0))
 
 # Visualization
+g.Set(conv)
 Draw(g, mesh, 'K*rho') # K \ast \rho
 Draw(s, mesh, 'rho') # \rho
+
+blub # Strange: g ist nicht konstant
 
 if vtkoutput:
     vtk = MyVTKOutput(ma=mesh,coefs=[s],names=["rho"],filename="nonlocaldiffusion",subdivision=3)
@@ -101,9 +107,23 @@ with TaskManager():
         
         a1.Assemble()
         a2.Assemble()
-        smat.AsVector().data = mmat.AsVector() + tau * a1.mat.AsVector()
-        rhs.data = mmat * s.vec - tau * a2.mat * s.vec
-        s.vec.data = smat.Inverse(fes.FreeDofs()) * rhs
+
+   	# Solve using explicit euler
+        a2.Apply (s.vec, q)
+#        fes.SolveM (rho=CoefficientFunction(1), vec=q)
+	
+        s1.data = s.vec - tau * (mmat.Inverse(fes.FreeDofs())*q) # Two stage SSP-RK
+
+        a2.Apply (s1, q)
+#        fes.SolveM (rho=CoefficientFunction(1), vec=q)
+       
+        s.vec.data = 0.5*(s.vec) + 0.5*(s1-tau*(mmat.Inverse(fes.FreeDofs())*q) ) # Two stage SSP-RK        
+#        
+
+
+#        smat.AsVector().data = mmat.AsVector() + 0*tau * a1.mat.AsVector()
+ #       rhs.data = mmat * s.vec - tau * a2.mat * s.vec
+  #      s.vec.data = smat.Inverse(fes.FreeDofs()) * rhs
 
         t += tau
         print("\n mass = {:10.6e}".format(Integrate(s,mesh)) +  "t = {:10.6e}".format(t))
