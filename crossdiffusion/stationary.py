c20bde3087c28906a2f3d16d07bce1f0a3b3ed17
Alexander Schlüter <alx.schlueter@gmail.com>
2017-04-13 00:59:01 +0200

    crossdiff: simplify stationary

--- crossdiffusion/stationary.py
+++ crossdiffusion/stationary.py
@@ -21,34 +21,8 @@ def AApply(uv, p, mesh, mat):
     a = (uv[0]-p.Vr)/p.Dr
     b = (uv[1]-p.Vb)/p.Db
 
-    # For very small diffusion coeffs Dr or Db, the exponents get too large.
-    # To prevent NaN's, we need to switch between equivalent expressions.
-
-    mmr = Integrate(IfPos(
-        -max(a, b),
-        exp(a)/(1+exp(a)+exp(b)),          # a <= 0 && b <= 0
-        IfPos(
-            a-b,
-            1/(1+exp(-a)+exp(b-a)),        # a >= b && a > 0
-            exp(a-b)/(1+exp(-b)+exp(a-b))  # b > a && b > 0
-            )
-        ), mesh, definedon=mat)
-
-    mmb = Integrate(IfPos(
-        -max(a, b),
-        exp(b)/(1+exp(a)+exp(b)),          # a <= 0 && b <= 0
-        IfPos(
-            a-b,
-            exp(b-a)/(1+exp(-a)+exp(b-a)), # a >= b && a > 0
-            1/(1+exp(-b)+exp(a-b))         # b > a && b > 0
-            )
-        ), mesh, definedon=mat)
-
-    mmr2 = Integrate(1 / (1+exp(-(uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db-(uv[0]-p.Vr)/p.Dr)), mesh, definedon=mat)
-    mmb2 = Integrate(1 / (1+exp((uv[0]-p.Vr)/p.Dr-(uv[1]-p.Vb)/p.Db)+exp(-(uv[1]-p.Vb)/p.Db)), mesh, definedon=mat)
-    err = sqrt((mmr-mmr2)**2+(mmb-mmb2)**2)
-    if err > 0:
-        print(err)
+    mmr = Integrate(1 / (1+exp(-a)+exp(b-a)), mesh, definedon=mat)
+    mmb = Integrate(1 / (1+exp(-b)+exp(a-b)), mesh, definedon=mat)
 
     return (np.hstack((mmr, mmb)))
 
@@ -57,6 +31,9 @@ def AssembleLinearization(uv, p, mesh, mat):
     b = (uv[1]-p.Vb)/p.Db
 
     m = np.empty([2,2])
+
+    # For very small diffusion coeffs Dr or Db, the exponents get too large.
+    # To prevent NaN's, we need to switch between equivalent expressions.
 
     m[0,0] = Integrate(IfPos(
         -max(a, b),
@@ -98,16 +75,16 @@ def AssembleLinearization(uv, p, mesh, mat):
             )
         ), mesh, definedon=mat)
 
-    m2 = np.empty([2,2])
-    m2[0,0] = Integrate(exp((uv[0]-p.Vr)/p.Dr)*(1+exp((uv[1]-p.Vb)/p.Db))/(sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db))*p.Dr),mesh,definedon=mat)
-    m2[0,1] = Integrate(-1/p.Db*exp((uv[0]-p.Vr)/p.Dr)*exp((uv[1]-p.Vb)/p.Db)/sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db)),mesh,definedon=mat)
+    # m2 = np.empty([2,2])
+    # m2[0,0] = Integrate(exp((uv[0]-p.Vr)/p.Dr)*(1+exp((uv[1]-p.Vb)/p.Db))/(sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db))*p.Dr),mesh,definedon=mat)
+    # m2[0,1] = Integrate(-1/p.Db*exp((uv[0]-p.Vr)/p.Dr)*exp((uv[1]-p.Vb)/p.Db)/sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db)),mesh,definedon=mat)
 
-    m2[1,0] = Integrate(-1/p.Dr*exp((uv[0]-p.Vr)/p.Dr)*exp((uv[1]-p.Vb)/p.Db)/sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db)),mesh,definedon=mat)
-    m2[1,1] = Integrate(exp((uv[1]-p.Vb)/p.Db)*(1+exp((uv[0]-p.Vr)/p.Dr))/(sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db))*p.Db),mesh,definedon=mat)
+    # m2[1,0] = Integrate(-1/p.Dr*exp((uv[0]-p.Vr)/p.Dr)*exp((uv[1]-p.Vb)/p.Db)/sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db)),mesh,definedon=mat)
+    # m2[1,1] = Integrate(exp((uv[1]-p.Vb)/p.Db)*(1+exp((uv[0]-p.Vr)/p.Dr))/(sqr(1+exp((uv[0]-p.Vr)/p.Dr)+exp((uv[1]-p.Vb)/p.Db))*p.Db),mesh,definedon=mat)
 
-    err = np.linalg.norm(m-m2)
-    if err > 0:
-        print(err)
+    # err = np.linalg.norm(m-m2)
+    # if err > 0:
+    #     print(err)
 
     return m
 
