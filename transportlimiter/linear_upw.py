47235d161006a698fc6bff22ad0d0856a8ba3de2
Jan-F. P <jan-fp@web.de>
2017-08-07 20:40:25 +0200

    added simple test mesh

--- transportlimiter/linear_upw.py
+++ transportlimiter/linear_upw.py
@@ -19,13 +19,14 @@ from ngsapps.plotting import *
 
 ngsglobals.msg_level = 1
 
-order = 3
-maxh = 0.05
-tau = 0.01
+order = 1
+maxh = 0.1
+tau = 0.005
 tend = -1
 
 usegeo = "circle"
 usegeo = "1d"
+usegeo = "test"
 
 if usegeo == "circle":
     geo = SplineGeometry()
@@ -33,10 +34,28 @@ if usegeo == "circle":
     netgenMesh = geo.GenerateMesh(maxh=maxh)
 elif usegeo == "1d":
     netgenMesh = Make1DMesh(-1, 1, maxh)
+elif usegeo == "test":
+    netgenMesh = NetMesh()
+    netgenMesh.dim = 1
+    start = -1
+    L = 2
+    N = 3 # Nof elements ?
+    pnums = []
+    for i in range(0, N + 1):
+        pnums.append(netgenMesh.Add(MeshPoint(Pnt(start + L * i / N, 0, 0))))
+
+    for i in range(0, N):
+        netgenMesh.Add(Element1D([pnums[i], pnums[i + 1]]))
+        # netmesh.SetMaterial(i+1, 'top'+str(i+1))
+        # netmesh.Add(Element1D([pnums[i], pnums[i + 1]], index=1))
+
+#    netgenMesh.Add(Element0D(pnums[0], index=1))
+#    netgenMesh.Add(Element0D(pnums[N], index=2))
+    #netmesh.SetMaterial(1, 'top')
+    #return netmesh
 
 mesh = Mesh(netgenMesh)
 #mesh.Curve(order)
-
 # finite element space
 fes = L2(mesh, order=order, flags={'dgjumps': True})
 v = fes.TrialFunction()
@@ -51,14 +70,14 @@ h = specialcf.mesh_size
 # velocity field and absorption
 if usegeo == "circle":
     beta = CoefficientFunction((1,0))
-elif usegeo == "1d":
+elif usegeo == "1d" or usegeo == "test":
     beta = CoefficientFunction(1)
     
 mu = 0.0
 
 # upwind fluxes scheme
 aupw = BilinearForm(fes)
-
+#sdfds
 # u_t + beta*grad(u) = 0
 aupw += SymbolicBFI(beta*grad(v)*w)
 aupw += SymbolicBFI(negPart(beta*n)*v*w, BND, skeleton=True)
@@ -100,7 +119,11 @@ f.Assemble()
 rhs = u.vec.CreateVector()
 mstar = aupw.mat.CreateMatrix()
 
-u.Set(exp(-(x*x+y*y)))
+if usegeo == "test":
+    u.vec[2] = 1
+    u.vec[3] = 1
+else:
+    u.Set(exp(-(x*x+y*y)))
 
 if netgenMesh.dim == 1:
     uplot = Plot(u, mesh=mesh)
@@ -118,15 +141,15 @@ with TaskManager():
         t += tau
 
         # Explicit
-#        rhs.data = m.mat * u.vec - tau * aupw.mat * u.vec
+        rhs.data = m.mat * u.vec - tau * aupw.mat * u.vec
 #        rhs.data += f.vec
-#        u.vec.data = minv * rhs
+        u.vec.data = minv * rhs
         
         # Implicit
-        rhs.data = m.mat * u.vec
-        mstar.AsVector().data = m.mat.AsVector() + tau * aupw.mat.AsVector()
-        invmat = mstar.Inverse(fes.FreeDofs())
-        u.vec.data = invmat * rhs
+#        rhs.data = m.mat * u.vec
+#        mstar.AsVector().data = m.mat.AsVector() + tau * aupw.mat.AsVector()
+#        invmat = mstar.Inverse(fes.FreeDofs())
+#        u.vec.data = invmat * rhs
 
         # Calculate mass
         print('mass = ' + str(Integrate(u,mesh)))
