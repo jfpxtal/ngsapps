7a34dc603969e0e60ef44cc8dfcdb501d26f1428
jfpxtal <jan-fp@web.de>
2017-08-09 00:05:42 +0200

    Add SIP for diffusion - not really working well, only for extremly small time step

--- transportlimiter/nonlin_upw.py
+++ transportlimiter/nonlin_upw.py
@@ -19,11 +19,13 @@ from rungekutta import *
 
 ngsglobals.msg_level = 1
 
-order = 3
-maxh = 0.01
-tau = 0.01
+order = 1
+maxh = 0.02
+tau = 0.0005
 tend = -1
 
+
+usegeo = "circle" 
 usegeo = "1d"
 
 if usegeo == "circle":
@@ -42,6 +44,7 @@ v = fes.TrialFunction()
 w = fes.TestFunction()
 
 u = GridFunction(fes)
+uc = GridFunction(fes)
 
 # special values for DG
 n = specialcf.normal(mesh.dim)
@@ -51,7 +54,7 @@ h = specialcf.mesh_size
 if usegeo == "circle":
     beta = CoefficientFunction((1,0))
 elif usegeo == "1d":
-    beta = CoefficientFunction(0.1)
+    beta = CoefficientFunction(1)
 
 mu = 0.0
 
@@ -77,11 +80,11 @@ etaf = abs(beta*n)
 phi = 0.5*(v*(1-v) + v.Other(0)*(1-v.Other(0)))*beta*n
 phi += 0.5*etaf*(v-v.Other(0))
 
-phiR = IfPos(beta*n, beta*n*IfPos(u-0.5, 0.25, u*(1-u)), 0)
+phiR = IfPos(beta*n, beta*n*IfPos(v-0.5, 0.25, v*(1-v)), 0)
 
 a += SymbolicBFI(-v*(1-v)*beta*grad(w))
-a += SymbolicBFI(phi*(w - w.Other()), skeleton=True)
-a += SymbolicBFI(phiR*w, BND, skeleton=True)
+a += SymbolicBFI(phi*(w - w.Other()), VOL, skeleton=True)
+#a += SymbolicBFI(phiR*w, BND, skeleton=True)
 
 # semi-implicit (not working yet, GridFunction.Other())
 # etaf = abs(beta*n)
@@ -92,7 +95,24 @@ a += SymbolicBFI(phiR*w, BND, skeleton=True)
 # a += SymbolicBFI(phi*(w - w.Other()), skeleton=True)
 # a += SymbolicBFI(phi*w, BND, skeleton=True) FIXME
 
-
+# Add diffusion
+D = 0.05
+eta = 3
+asip = BilinearForm(fes)
+asip += SymbolicBFI(D*grad(v)*grad(w))
+asip += SymbolicBFI(-D*0.5*(grad(v)+grad(v.Other())) * n * (w - w.Other()), skeleton=True)
+asip += SymbolicBFI(-D*0.5*(grad(w)+grad(w.Other())) * n * (v - v.Other()), skeleton=True)
+asip += SymbolicBFI(D*eta / h * (v - v.Other()) * (w - w.Other()), skeleton=True)
+    
+Dirichlet = True
+if Dirichlet:
+    asip += SymbolicBFI(-D*0.5*(grad(v)) * n * (w), BND, skeleton=True) #, definedon=topMat)
+    asip += SymbolicBFI(-D*0.5*(grad(w)) * n * (v), BND, skeleton=True) #, definedon=topMat)
+    asip += SymbolicBFI(D*eta / h * (v) * w, BND, skeleton=True) #, definedon=topMat)
+    
+    
+asip.Assemble()
+    
 # mass matrix
 m = BilinearForm(fes)
 m += SymbolicBFI(v*w)
@@ -109,19 +129,26 @@ minv = m.mat.Inverse(fes.FreeDofs())
 # f.Assemble()
 
 rhs = u.vec.CreateVector()
-# mstar = m.mat.CreateMatrix()
+rhs2 = u.vec.CreateVector()
+mstar = m.mat.CreateMatrix()
 
 u.Set(0.9*exp(-2*(x*x+y*y)))
+uc.Set(0.5+0*x)
 # u.Set(CoefficientFunction(0.4))
 
 if netgenMesh.dim == 1:
     uplot = Plot(u, mesh=mesh, subdivision=3)
+    ucplot = Plot(uc, mesh=mesh, subdivision=3)
+    plt.axis([-1, 1, 0, 1])
     plt.show(block=False)
 else:
     Draw(u, mesh, 'u')
 
 def step(t, y):
-    a.Apply(y, rhs)
+    a.Apply(y,rhs)
+    asip.Apply(y, rhs2)
+    rhs.data += rhs2
+#    rhs.data = asip.mat * y
     fes.SolveM(rho=CoefficientFunction(1), vec=rhs)
     return -rhs
 
@@ -143,15 +170,15 @@ with TaskManager():
         # TODO: limit after each interior Euler step!
         u.vec.data = RungeKutta(rk4, tau, step, t, u.vec)
 
-        # a.Apply(u.vec, rhs)
-        # fes.SolveM(rho=CoefficientFunction(1), vec=rhs)
-        # u.vec.data -= tau*rhs
+#        asip.Apply(u.vec, rhs)
+#        fes.SolveM(rho=CoefficientFunction(1), vec=rhs)
+#        u.vec.data -= tau*rhs
 
         # Implicits
-        # rhs.data = m.mat * u.vec
-        # mstar.AsVector().data = m.mat.AsVector() + tau * a.mat.AsVector()
-        # invmat = mstar.Inverse(fes.FreeDofs())
-        # u.vec.data = invmat * rhs
+#        rhs.data = m.mat * u.vec
+#        mstar.AsVector().data = m.mat.AsVector() + tau * asip.mat.AsVector()
+#        invmat = mstar.Inverse(fes.FreeDofs())
+#        u.vec.data = invmat * rhs
 
         stabilityLimiter(u, fes, uplot)
         nonnegativityLimiter(u, fes, uplot)
@@ -160,7 +187,7 @@ with TaskManager():
         print('mass = ' + str(Integrate(u,mesh)))
 
         if netgenMesh.dim == 1:
-            if k % 20 == 0:
+            if k % 50 == 0:
                 uplot.Redraw()
                 plt.pause(0.001)
                 # input()
