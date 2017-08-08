5e71490eea59a4d2a44d514dad437fea481ce6fc
Alexander Schlüter <alx.schlueter@gmail.com>
2017-08-08 15:27:21 +0200

    Lax-Friedrichs

--- transportlimiter/nonlin_upw.py
+++ transportlimiter/nonlin_upw.py
@@ -8,25 +8,21 @@ u_t + div(u(1-u)\beta) = 0
 
 @author: pietschm
 """
-from netgen.meshing import Element0D, Element1D, MeshPoint, Mesh as NetMesh
-from netgen.csg import Pnt
 from netgen.geom2d import SplineGeometry
 from ngsolve import *
-from ngsapps.utils import *
 import numpy as np
 
-#from geometries import *
+from ngsapps.utils import *
 from ngsapps.plotting import *
 from limiter import *
 
 ngsglobals.msg_level = 1
 
 order = 1
-maxh = 0.05
+maxh = 0.01
 tau = 0.001
 tend = -1
 
-usegeo = "circle"
 usegeo = "1d"
 
 if usegeo == "circle":
@@ -59,33 +55,38 @@ elif usegeo == "1d":
 mu = 0.0
 
 # upwind fluxes scheme
-aupw = BilinearForm(fes)
+a = BilinearForm(fes)
+
+# # centered trial
+# a += SymbolicBFI((1-2*u)*beta*grad(v)*w)
+# a += SymbolicBFI(negPart((1-2*u)*beta*n)*v*w, BND, skeleton=True)
+# a += SymbolicBFI(-(1-2*u)*beta*n*(v - v.Other())*0.5*(w + w.Other()), skeleton=True)
+
+# # centered test
+# a += SymbolicBFI(-(1-2*u)*beta*v*grad(w))
+# a += SymbolicBFI(negPart((1-2*u)*beta*n)*v*w, BND, skeleton=True)
+# a += SymbolicBFI(-(1-2*u)*beta*n*(v - v.Other())*0.5*(w + w.Other()), skeleton=True)
 
 # u_t + beta*grad(u) = 0
-# aupw += SymbolicBFI((1-2*u)*beta*grad(v)*w)
-# aupw += SymbolicBFI(negPart((1-2*u)*beta*n)*v*w, BND, skeleton=True)
-# aupw += SymbolicBFI(-(1-2*u)*beta*n*(v - v.Other())*0.5*(w + w.Other()), skeleton=True)
-# aupw += SymbolicBFI(0.5*abs((1-2*u)*beta*n)*(v - v.Other())*(w - w.Other()), skeleton=True)
+# a += SymbolicBFI((1-2*u)*beta*grad(v)*w)
+# a += SymbolicBFI(negPart((1-2*u)*beta*n)*v*w, BND, skeleton=True)
+# a += SymbolicBFI(-(1-2*u)*beta*n*(v - v.Other())*0.5*(w + w.Other()), skeleton=True)
+# a += SymbolicBFI(0.5*abs((1-2*u)*beta*n)*(v - v.Other())*(w - w.Other()), skeleton=True)
 
 # u_t + div(beta*u) = 0
-# aupw += SymbolicBFI(-v*(1-u)*beta*grad(w))
-# aupw += SymbolicBFI(posPart((1-u)*beta*n)*v*w, BND, skeleton=True)
-# aupw += SymbolicBFI((1-u)*beta*n*(v + v.Other())*0.5*(w - w.Other()), skeleton=True)
-# aupw += SymbolicBFI(0.5*abs((1-u)*beta*n)*(v - v.Other())*(w - w.Other()), skeleton=True)
+# a += SymbolicBFI(-v*(1-2*u)*beta*grad(w))
+# a += SymbolicBFI(posPart((1-2*u)*beta*n)*v*w, BND, skeleton=True)
+# a += SymbolicBFI((1-2*u)*beta*n*(v + v.Other())*0.5*(w - w.Other()), skeleton=True)
+# a += SymbolicBFI(0.5*10*abs((1-2*u)*beta*n)*(v - v.Other())*(w - w.Other()), skeleton=True)
 
-# Lax Friedrich
+# Lax Friedrichs
 etaf = abs(beta*n)
-phi = 0.5*(v*(1-u)*beta*n + v.Other()*(1-u.Other())*beta*n)
-phi += etaf*(v-v.Other())
+phi = 0.5*(v*(1-v) + v.Other(0)*(1-v.Other(0)))*beta*n
+phi += 0.5*etaf*(v-v.Other(0))
 
-aupw += SymbolicBFI(-v*(1-u)*beta*grad(w))
-aupw += SymbolicBFI(phi*(w - w.Other()), skeleton=True)
-
-phib = 0.5*(v*(1-u)*beta*n + 0)
-phib += etaf*(v-0)
-aupw += SymbolicBFI(phib*(w - w.Other()), BND, skeleton=True)
-# aupw += SymbolicBFI(negPart(beta*n)*v*w, BND, skeleton=True)
-
+a += SymbolicBFI(-v*(1-v)*beta*grad(w))
+a += SymbolicBFI(phi*(w - w.Other()), skeleton=True)
+a += SymbolicBFI(phi*w, BND, skeleton=True)
 
 
 # mass matrix
@@ -95,21 +96,21 @@ m += SymbolicBFI(v*w)
 f = LinearForm(fes)
 f += SymbolicLFI(0 * w)
 
-print('Assembling aupw...')
-aupw.Assemble()
+# print('Assembling a...')
+# a.Assemble()
 print('Assembling m...')
 m.Assemble()
 minv = m.mat.Inverse(fes.FreeDofs())
-print('Assembling f...')
-f.Assemble()
+# print('Assembling f...')
+# f.Assemble()
 
 rhs = u.vec.CreateVector()
-mstar = aupw.mat.CreateMatrix()
+# mstar = m.mat.CreateMatrix()
 
-u.Set(0.9*exp(-(x*x+y*y)))
+u.Set(0.9*exp(-2*(x*x+y*y)))
 
 if netgenMesh.dim == 1:
-    uplot = Plot(u, mesh=mesh)
+    uplot = Plot(u, mesh=mesh, subdivision=3)
     plt.show(block=False)
 else:
     Draw(u, mesh, 'u')
@@ -124,32 +125,37 @@ with TaskManager():
         print("\nt = {:10.6e}".format(t))
         t += tau
         k += 1
-        
-        aupw.Assemble()
+
+        # a.Assemble()
 
         # Explicit
-#        aupw.Apply (u.vec, rhs)
-#        rhs.data = (-1*tau)*rhs
-#        rhs.data += m.mat * u.vec
-        rhs.data = m.mat * u.vec - tau * aupw.mat * u.vec
-        rhs.data += f.vec
-        u.vec.data = minv * rhs
-        
+        a.Apply(u.vec, rhs)
+        fes.SolveM(rho=CoefficientFunction(1), vec=rhs)
+        u.vec.data -= tau*rhs
+        # rhs.data = (-1*tau)*rhs
+        # rhs.data += m.mat * u.vec
+        # rhs.data = m.mat * u.vec - tau * a.mat * u.vec
+        # rhs.data += f.vec
+        # u.vec.data = minv * rhs
+
         # Implicits
 #        rhs.data = m.mat * u.vec
-#        mstar.AsVector().data = m.mat.AsVector() + tau * aupw.mat.AsVector()
+#        mstar.AsVector().data = m.mat.AsVector() + tau * a.mat.AsVector()
 #        invmat = mstar.Inverse(fes.FreeDofs())
 #        u.vec.data = invmat * rhs
+
         # stabilityLimiter(u, fes, uplot)
         # nonnegativityLimiter(u, fes, uplot)
+
         # Calculate mass
         print('mass = ' + str(Integrate(u,mesh)))
 
         if netgenMesh.dim == 1:
-            if k % 100 == 0:
+            if k % 10 == 0:
                 uplot.Redraw()
                 plt.pause(0.001)
                 input()
         else:
             Redraw(blocking=False)
+
 
