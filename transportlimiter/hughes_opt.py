hughes: fix xi derivative, functional now decreasing

transportlimiter/hughes_opt.py
@@ -24,9 +24,6 @@ import pickle
 
 ngsglobals.msg_level = 0
 
-def abs(x):
-    return IfPos(x, x, -x)
-
 def f(u):
     return 1-u
 
@@ -118,14 +115,12 @@ def EikonalSolver():
 
         # Apply nonlinear operator
         phi_rhs.vec.data = phi.vec
-        #aeupw.Assemble()
-        #a.Apply(phi.vec, q)
-        aeupw.Apply(phi.vec, q)
+        aeupw.Assemble()
+        q.data = aeupw.mat*phi.vec
         q.data += a.mat*phi.vec
         q.data -= feik.vec
 
         # Linearized HJ-Operator - has additional 2 from square
-        aeupw.Assemble()
         mstar.AsVector().data = a.mat.AsVector() + 2*aeupw.mat.AsVector()
 
         # Solve for update and perform Newton step        
@@ -170,7 +165,7 @@ def HughesSolver(vels):
     # Initial data
     mi = 1# Integrate(unitial, mesh)
     u.Set(1/mi*unitial)
-    agents = np.array([0.0]) # Initial pos agents
+    agents = np.array([5]) # Initial pos agents
     phi.Set(5-abs(x))
     rhodata = []
     phidata = []
@@ -251,6 +246,8 @@ fadj2 += SymbolicLFI(-u*f(u)*f(u)*grad(lam1)*grad(w)) # FIXME
 
 invmat2 = del1*asip.mat.Inverse(fes.FreeDofs())
 
+gradlam1 = GridFunction(fes)
+
 def AdjointSolver(rhodata, phidata, agentsdata):
     t = 0.0
     k = 0
@@ -290,12 +287,13 @@ def AdjointSolver(rhodata, phidata, agentsdata):
         rhs.data += tau*fadj2.vec
         lam2.vec.data = invmat2 * rhs
 
+        gradlam1.Set(grad(lam1))
         # Integrate lam3-equation
         for i in range(0,agents.size):
             norm = sqrt(sqr(x-agents[0])+y*y)
             K = cK*posPart(1-norm/width)
             g.Set(K)
-            upd = (1/Na)*Integrate(u*sqr(f(u))*grad(g)*grad(lam1), mesh)
+            upd = (1/Na)*Integrate((grad(u)*grad(lam1)*(sqr(f(u))+2*u*f(u)*fprime(u))+u*sqr(f(u))*grad(gradlam1))*grad(g), mesh)
             lam3[i] = lam3[i] - tau*upd
             vels[i,k] = -Na*tend/alpha*lam3[i]
 
@@ -313,7 +311,7 @@ def AdjointSolver(rhodata, phidata, agentsdata):
             Redraw(blocking=False)
 
         k += 1
-    return [vels[:,::-1], Vs]
+    return [vels[:,::-1], Vs[::-1]]
 
 if vtkoutput:
     vtk = MyVTKOutput(ma=mesh,coefs=[u, phi],names=["rho","phi"],filename="vtk/rho",subdivision=1)
@@ -361,7 +359,7 @@ else:
     Draw(lam2, mesh, 'lam2')
 
 # Gradient descent
-Nopt = 10
+Nopt = 20
 otau = 0.1
 
 #sad
@@ -416,8 +414,6 @@ with TaskManager():
         plt.pause(0.001)    
         
         # Update velocities 
-        # TODO: Projection auf [-minvel, maxvel]
-         #   urhl
         vels = vels - otau*nvels
         
         # Project to interval [-radius/tend, radius/tend]